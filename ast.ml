bd51dbd208785ea9b672d8b6c6fd28774bc68d5a
physikks <94301389+physikks@users.noreply.github.com>
2021-11-14 18:38:08 +0100

    Update ast.ml

--- ast.ml
+++ ast.ml
@@ -1,9 +1,55 @@
 (* Syntaxe abstraite pour petit Java *)
+type ident = string
 
+type ntype = Ntype of ident * ntype list
 
-type typ = boolean | I
+type jtype = 
+	|	Jboolean | Jint 
+	| Jntype of ntype
 
+type unop =
+  | Uneg (* -e *)
+  | Unot (* not e *)
 
+type binop =
+  | Badd | Bsub | Bmul | Bdiv | Bmod    (* + - * / % *)
+  | Beq | Bneq | Blt | Ble | Bgt | Bge  (* == != < <= > >= *)
+  | Band | Bor                          (* && || *)
+
+type expr =
+	|	Enil
+	|	Esimple of expr_simple
+	|	Eequal of acces * expr
+	|	Eunop of unop * expr
+	|	Ebinop of expr * binop * expr
+
+and acces = 
+	|	Aident of ident
+	|	Achemin of expr_simple * ident
+
+and expr_simple =
+	|	ESint of int	| ESstr of string	| ESstr of bool
+	|	ESthis
+	|	ESexpr of expr
+	|	ESnew of ntype * expr list
+	|	ESacces of acces * expr list
+
+type instruction = 
+	| Inil
+	|	Isimple of expr_simple
+	|	Idef of acces * expr
+	|	Iinit of jtype * ident
+	|	Iinit_def of jtype * ident * expr
+	|	Iif of expr * instruction * instruction
+	|	Iwhile of expr * instruction
+	|	Ibloc of instruction list
+	| Ireturn of expression option
+
+type param = jtype * ident
+
+type proto =
+
+type classe_main = ident * instruction list 
 
 type fichier = class_intf list * classe_main
 

c1046331c0b121206fef939d23e386637da0d4a6
Samuel Coulomb <84394386+Samsamitos@users.noreply.github.com>
2021-12-19 17:32:22 +0100

    Operateurs += et +++, map cls et var en argument

--- code_prod.ml
+++ code_prod.ml
@@ -3,6 +3,9 @@ open Ast
 open Ast_typing
 open X86_64
 
+let (+=) (t1, d) t2 = t1 ++ t2, d
+let (+++) (t1, d1) (t2, d2) = t1 ++ t2, d1 ++ d2 
+
 let ntl = ref 0
 let new_text_label () =
   incr ntl ;
@@ -13,84 +16,84 @@ let new_data_label () =
   incr ndl ;
   Format.sprintf "D%d" !ntl s
 
-let rec cp_expr e = match e with
+let rec cp_expr cls var e = match e with
   | Enull ->
-  | Esimple es -> cp_expr_simple es
+  | Esimple es -> cp_expr_simple cls var es
   | Eequal (a, e) ->                     
-  | Eunop (Uneg, e) -> let t, d = cp_expr e in
-                       t ++ negq (reg rax), d 
-  | Eunop (Unot ,e) -> let t, d = cp_expr e in
-                       t ++ notq (reg rax), d
+  | Eunop (Uneg, e) -> cp_expr cls var e += negq (reg rax)
+  | Eunop (Unot ,e) -> cp_expr cls var e += notq (reg rax)
   | Ebinop (Beq | Bneq | Blt | Ble | Bgt | Bge as op, e1, e2) ->  
-      let t1, d1 = cp_expr e1 in
-			let t2, d2 = cp_expr e2 in
-			t1 ++ movq (reg rax) (red rdi) ++
-			t2 ++ cmpq (reg rax) (reg rdi) ++
+      cp_expr cls var e1 +=
+			movq (reg rax) (red rdi) +++
+			cp_expr cls var e2 +=
+			cmpq (reg rax) (reg rdi) +=
 			begin match op with
 				| Beq -> sete | Bneq -> setne
 				| Blt -> setl | Ble -> setle
 				| Bgt -> setg | Bge -> setge
-			end (reg rax), d1 ++ d2
+			end (reg rax)
 	| Ebinop (Badd | Bsub | Bmul | Bdiv | Bmod as op, e1 , e2) ->
-			let t1, d1 = cp_expr e1 in
-			let t2, d2 = cp_expr e2 in
-			t1 ++ pushq (reg rax) ++
-			t2 ++ movq (reg rax) (reg rdi) ++ popq (reg rax) ++
+			cp_expr cls var e1 +=
+			pushq (reg rax) +++
+			cp_expr cls var e2 +=
+			movq (reg rax) (reg rdi) +=
+			popq (reg rax) +=
 			begin match op with
 				| Badd -> addq (reg rdi) (reg rax)
 				| Bsub -> subq (reg rdi) (reg rax)
 				| Bmul -> imulq (reg rdi) (reg rax)
 				| Bdiv -> idivq (reg rdi)
-				| Bmod -> idivq (reg rdi) ++ movq (reg rdx) (reg rax)
-			end, d1 ++ d2
+				| Bmod -> idivq (reg rdi) ++ movq (reg rdx) (reg rax) end
 	| Ebinop (Band | Bor as op, e1, e2) ->
-			let t1, d1 = cp_expr e1 in
-			let t2, d2 = cp_expr e2 in
 			let lbl = new_text_label () in
-			t1 ++ testq (reg rax) (reg rax) ++
-			(match op with | Band -> je | Bor -> jne) lbl ++
-			t2 ++ label lbl, d1 ++ d2
-and cp_expr_simple es = match es with
+			cp_expr cls var e1 +=
+			testq (reg rax) (reg rax) +=
+			(match op with | Band -> je | Bor -> jne) lbl +++
+			cp_expr cls var e2 +=
+			label lbl
+and cp_expr_simple cls var es = match es with
   | ESint n -> movq (imm n) (reg rax), nop
   | ESbool b -> movq (imm (if b then 1 else 0)) (reg rax), nop
   | ESstr s -> let lbl = new_data_label () in
                movq (lab lbl) (reg rax), label_lab ++ string s
   | ESthis ->
-  | ESexpr e -> cp_expr e
+  | ESexpr e -> cp_expr cls var e
   | ESnew (nt, l)  ->
   | ESacces_meth (a, l) ->
   | ESacces_var a ->
-and cp_acces a = match a with
+and cp_acces cls var a = match a with
 	| Aident id ->
 	| Achemin (es, id) ->
 
-let rec cp_instruction st = match st with
+let rec cp_instruc cls var st = match st with
 	| Inil -> nop, nop
-	| Isimple es -> cp_expr_simple es
+	| Isimple es -> cp_expr_simple cls var es
 	| Iequal (a, e) ->
 	| Idef (jt, id) ->
 	| Idef_init (jt, id, e) ->
 	| Iif (e, s1, s2) ->
-			let t0, d0 = cp_expr e in
-			let t1, d1 = cp_instruction s1 in
-			let t2, d2 = cp_instruction s2 in
 			let lbl1 = new_label_text () in
 			let lbl2 = new_label_text () in
-			t0 ++ testq (reg rax) (reg rax) ++ je lbl2 ++
-			t1 ++ jmp lbl1 ++ lab lbl2 ++
-			t2 ++ lab lbl1,	d0 ++ d1 ++ d2
+			cp_expr cls var e +=
+			testq (reg rax) (reg rax) +=
+			je lbl2 +++
+			cp_instruc cls var s1 +=
+			jmp lbl1 +=
+			lab lbl2 +++
+			cp_instruc cls var s2 +=
+			lab lbl1
 	| Iwhile (e, s) ->
-			let te, de = cp_expr e in
-			let ts, ds = cp_instruction s in
 			let lbl1 = new_label_text () in
 			let lbl2 = new_label_text () in
-			lab lbl1 ++ te ++ testq (reg rax) (reg rax) ++
-			je lbl2 ++ ts ++ jmp lbl1 ++ lab lbl2, de ++ ds
-	| Ibloc l -> let aux (t, d) s =
-								 let ti, di = cp_instruction s in
-								 t ++ ti, d ++ di
-							 in List.fold aux (nop, nop) l
-	| Ireturn opt -> let t, d = match opt with
-										 | None -> nop, nop
-										 | Some e -> cp_expr e
-									 in t ++ leave ++ ret, d										
+			(lab lbl1, nop) +++
+			cp_expr cls var e +=
+			testq (reg rax) (reg rax) +=
+			je lbl2 +++
+			cp_instruc cls var s +=
+			jmp lbl1 +=
+			lab lbl2
+	| Ibloc l -> let aux cd s = cd +++ cp_instruc cls var s
+							 in List.fold_left aux (nop, nop) l
+	| Ireturn None -> (nop, nop) += leave += ret
+	| Ireturn (Some e) -> cp_expr cls var e += leave += ret
+									 

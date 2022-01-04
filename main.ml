55ff7e1790cc4270fb43b022a273b6ff943dd1ba
Samuel Coulomb <84394386+Samsamitos@users.noreply.github.com>
2022-01-04 16:12:30 +0100

    Update main.ml

--- main.ml
+++ main.ml
@@ -56,9 +56,7 @@ let () =
     in
     if !type_only then exit 0 ;
     
-    (*
     let code = Compiler.cp_fichier typed in
     let file_s = (Filename.chop_suffix file_java ".java")^".s" in
     X86_64.print_in_file file_s ;
-    *)
     exit 0
